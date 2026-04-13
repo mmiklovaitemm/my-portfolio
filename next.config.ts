03d42e9bd89757f85c93254a5afb500572b855e4
Ugnė Miklovaitė <136790656+mmiklovaitemm@users.noreply.github.com>
2026-04-13 18:04:46 +0300

    Add GitHub Actions deployment workflow

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+
+  basePath: "/my-portfolio",
+  assetPrefix: "/my-portfolio",
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
